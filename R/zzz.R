e2a3ca0c5f02f81775ab0c1d730c2d0f3e7f6ec1
Stefano Maria Iacus <stefano.iacus@unimi.it>
2011-06-10 00:00:00 +0000

    version 1.0

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.noGenerics <- TRUE
+
+.First.lib <- function(libname, pkgname)
+{
+ cat("Companion package to the book:\n")
+ cat("Option Pricing and Estimation of Financial Models in R,\nWiley, Chichester, S.M. Iacus (2011)\n")
+ cat("\nFor more informations type: vignette(\"opefimor\") or ?opefimor\n")
+}
